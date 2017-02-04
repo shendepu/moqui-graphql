f6bf542de05bda9e0881713d666a4eec0e07a10c
SHEN, JIMMY <shendepu@hotmail.com>
2017-02-04 21:51:50 +0800

    Added DateRangeInputType and OperationInputType class for service parameter

--- src/main/groovy/com/moqui/graphql/DateRangeInputType.groovy
+++ src/main/groovy/com/moqui/graphql/DateRangeInputType.groovy
@@ -1,22 +1,26 @@
 package com.moqui.graphql
 
-class DateRangeInputType {
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class DateRangeInputType extends LinkedHashMap {
     public String period
     public String poffset
     public String from
     public String thru
 
     DateRangeInputType(Map m) {
-        this.period = m.period
-        this.poffset = m.poffset
-        this.from = m.from
-        this.thru = m.thru
+        this.period = m.period as String
+        this.poffset = m.poffset as String
+        this.from = m.from as String
+        this.thru = m.thru as String
+        if (this.period != null) this.put("period", this.period)
+        if (this.poffset != null) this.put("poffset", this.poffset)
+        if (this.from != null) this.put("from", this.from)
+        if (this.thru != null) this.put("thru", this.thru)
     }
 
     DateRangeInputType(String period, String poffset, String from, String thru) {
-        this.period = period
-        this.poffset = poffset
-        this.from = from
-        this.thru = thru
+        this([period: period, poffset: poffset, from: from, thru: thru])
     }
 }
--- src/main/groovy/com/moqui/graphql/OperationInputType.groovy
+++ src/main/groovy/com/moqui/graphql/OperationInputType.groovy
@@ -1,22 +1,27 @@
 package com.moqui.graphql
 
-class OperationInputType {
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class OperationInputType extends LinkedHashMap {
     public String value
     public String op
     public String not
     public String ic
 
     OperationInputType(Map m) {
-        this.value = m.value
-        this.op = m.op
-        this.not = m.not
-        this.ic = m.ic
+        this.value = m.value as String
+        this.op = m.op as String
+        this.not = m.not as String
+        this.ic = m.ic as String
+
+        if (value != null) this.put("value", value)
+        if (op != null) this.put("op", op)
+        if (not != null) this.put("not", not)
+        if (ic != null) this.put("ic", ic)
     }
 
     OperationInputType (String value, String op, String not, String ic) {
-        this.value = value
-        this.op = op
-        this.not = not
-        this.ic = ic
+        this([value: value, op: op, not: not, ic: ic])
     }
 }
